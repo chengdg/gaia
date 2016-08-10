3815fe4c089bb7fbb9d5db009b6e28285dbedf1e
robert <chenru@weizoom.com>
2016-08-10 16:56:00 +0800

    改进注册服务脚本

--- register_service/register_service.sh
+++ register_service/register_service.sh
@@ -1,2 +1,2 @@
-etcdctl set /service/weapp/service1 "127.0.0.1:8002"
-etcdctl set /extra/weapp/location1 "location /static { root /home/weizoom/microservice/launcher/new_zeus/; }"
+etcdctl set /service/new_zeus/service1 "127.0.0.1:8002"
+etcdctl set /extra/new_zeus/location1 "location /static { root /home/weizoom/microservice/launcher/new_zeus/; }"
