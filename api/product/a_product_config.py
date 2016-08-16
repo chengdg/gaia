55655f83b826210d11965f623ba0d4f12dd8c31a
朱天琦 <zhutianqi@weizoom.com>
2016-08-16 15:58:51 +0800

    update

--- api/product/a_product_config.py
+++ api/product/a_product_config.py
@@ -4,9 +4,7 @@
 from eaglet.core import api_resource
 from eaglet.decorator import param_required
 
-from business.mall.product import Product
 from business.mall.product_config import ProductConfig
-from business.mall.product_factory import ProductFactory
 
 
 class AProductConfig(api_resource.ApiResource):
---
+++ business/mall/owner.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import json
+
+from eaglet.core import watchdog
+from eaglet.decorator import param_required
+from eaglet.utils.resource_client import Resource
+
+from business import model as business_model
+from db.account import models as account_model
+from db.mall import models as mall_models
+
+
+class Owner(business_model.Model):
+	"""
+	云商通系统中的webapp_owner
+	"""
+
+	__slots__ = (
+		'id',
+		'username',
+		'mall_type',
+		'config',
+		'store_name'
+	)
+
+	def __init__(self, owner_id):
+		_user = account_model.User.select().dj_where(id=owner_id).first()
+		_user_profile = account_model.UserProfile.select().dj_where(user_id=owner_id).first()
+
+		self.id = owner_id
+		self.username = _user.username
+		self.mall_type = _user_profile.mall_type
+		self.store_name = _user_profile.store_name
