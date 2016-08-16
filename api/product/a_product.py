6c5096aa1ec5f142a9e48d02a4ad683e49df8e55
朱天琦 <zhutianqi@weizoom.com>
2016-08-16 09:59:01 +0800

    update

--- api/product/a_product.py
+++ api/product/a_product.py
@@ -22,43 +22,44 @@ class AProduct(api_resource.ApiResource):
 		@return:
 		"""
 
-		product_data = {
-			'postage_type': args['postage_type'],
-			'postage_id': args['postage_id'],
-			'unified_postage_money': args.get('unified_postage_money', 0.0),
-			'min_limit': float(args['min_limit']),
-			'purchase_price': float(args['purchase_price']),
-			'is_enable_bill': args['is_enable_bill'],
-			'is_delivery': args['is_delivery'],
-			'is_bill': args['is_bill'],
-			'owner_id': args['owner_id'],
-			'name': args['name'],
-			'promotion_title': args['promotion_title'],
-			'user_code': args['user_code'],
-			'bar_code': args['bar_code'],
-			'thumbnails_url': args['thumbnails_url'],
-			'pic_url': args['pic_url'],
-
-			'detail': args['detail'],
-			'is_use_online_pay_interface': args['is_use_online_pay_interface'],
-
-			'is_use_cod_pay_interface': args['is_use_cod_pay_interface'],
-
-			'is_member_product': args['is_member_product'],
-			'supplier': args['supplier'],
-
-			'swipe_images': args.get('swipe_images'),
-
-			# 商品规格数据
-			'is_use_custom_model': args['is_use_custom_model'],
-			'customModels': args.get('customModels', '[]'),
-
-			# 商品分组数据
-			'product_category': args['product_category'],
-
-			# 商品属性数据
-			'properties': args['properties', '[]']
-		}
+		# product_data = {
+		# 	'postage_type': args['postage_type'],
+		# 	'postage_id': args['postage_id'],
+		# 	'unified_postage_money': args.get('unified_postage_money', 0.0),
+		# 	'min_limit': float(args['min_limit']),
+		# 	'purchase_price': float(args['purchase_price']),
+		# 	'is_enable_bill': args['is_enable_bill'],
+		# 	'is_delivery': args['is_delivery'],
+		# 	'is_bill': args['is_bill'],
+		# 	'owner_id': args['owner_id'],
+		# 	'name': args['name'],
+		# 	'promotion_title': args['promotion_title'],
+		# 	'user_code': args['user_code'],
+		# 	'bar_code': args['bar_code'],
+		# 	'thumbnails_url': args['thumbnails_url'],
+		# 	'pic_url': args['pic_url'],
+		#
+		# 	'detail': args['detail'],
+		# 	'is_use_online_pay_interface': args['is_use_online_pay_interface'],
+		#
+		# 	'is_use_cod_pay_interface': args['is_use_cod_pay_interface'],
+		#
+		# 	'is_member_product': args['is_member_product'],
+		# 	'supplier': args['supplier'],
+		#
+		# 	'swipe_images': args.get('swipe_images'),
+		#
+		# 	# 商品规格数据
+		# 	'is_use_custom_model': args['is_use_custom_model'],
+		# 	'customModels': args.get('customModels', '[]'),
+		#
+		# 	# 商品分组数据
+		# 	'product_category': args['product_category'],
+		#
+		# 	# 商品属性数据
+		# 	'properties': args['properties', '[]']
+		# }
+		product_data = args
 		product_factory = ProductFactory.get()
 		product_factory.create_product(args['owner_id'],product_data)
 
