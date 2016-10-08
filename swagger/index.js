a5df508c3792e012822e31b9d4a5bd9a1ec09bb8
zhangxiangjiang <zhangxiangjiang@weizoom.com>
2016-10-08 12:02:20 +0800

    合并swagger文档 脚本

---
+++ swagger/index.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var path = require('path');
+var swaggermerge = require('swagger-merge');
+
+var sourceFolder = __dirname;
+var outputFolder = __dirname;
+var outputFileName = 'openapi.json';
+
+var basePath = '/api';
+var host = 'hermes.com';
+var schemes =  ['http'];
+
+// basePath = '';
+host = '';
+// schemes = [];
+
+
+
+
+var info = {
+    "version": "1.0.0",
+    "title": "Hermes Product Category API"
+};
+
+
+swaggermerge.on('warn', function (msg) {
+    console.warn(msg)
+});
+
+// 扫描json 文件
+console.log("开始扫描json 文件=================================");
+
+var swaggerJsonFiles = fs.readdirSync(sourceFolder).filter(function (file) {
+    return /\.json$/.test(file) && outputFileName != file;
+}).map(function (file) {
+	var filePath = path.join(sourceFolder,file);
+	console.log(filePath);
+    return require(filePath);
+});
+
+console.log("结束扫描json文件=================================");
+
+
+var merged = swaggermerge.merge(swaggerJsonFiles, info, basePath, host, schemes);
+
+var data = JSON.stringify(merged, null, 4);
+
+var outputFile = path.join(outputFolder, outputFileName);
+
+console.log("输出文件：" + outputFile);
+
+fs.writeFile(outputFile, data);
+
