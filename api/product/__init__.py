591871d50430404ecb3420b9899b14ab9de4ba6f
robert <chenru@weizoom.com>
2016-10-10 16:38:50 +0800

    删除不需要的文件

--- api/product/__init__.py
+++ api/product/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-import a_product_property
 import a_product
-import a_product_sales
-import a_product_status
-import a_pool_product
 import a_product_creation_option
 
 import property_template
