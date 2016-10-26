6457b59a424ffacebf46a182b9325beb26148be9
Eugene <237192896@qq.com>
2016-10-26 12:24:48 +0800

    添加获取平台省市地区的信息接口

---
+++ api/mall/logistics/a_province_city.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from eaglet.core import api_resource
+from eaglet.decorator import param_required
+
+
+class AProvinceCity(api_resource.ApiResource):
+	"""
+	获取平台的省市信息
+	"""
+	app = 'mall'
+	resource = 'province_city'
+
+	@param_required(['corp_id'])
+	def get(args):
+		corp = args['corp']
+		zones = corp.province_city_repository.get_zones()
+
+		return {'zones': zones}
