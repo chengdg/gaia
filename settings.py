8ab038e59aa0c3dd27dfdd6b0a4521d2d6eea1cc
朱天琦 <zhutianqi@weizoom.com>
2016-10-28 18:45:03 +0800

    测试topic:new-zeus-test->test-topic

--- settings.py
+++ settings.py
@@ -165,7 +165,7 @@ else:
     MNS_ACCESS_KEY_SECRET = 'bPKU71c0cfrui4bWgGPO96tLiOJ0PZ'
     MNS_ENDPOINT = 'https://1615750970594173.mns.cn-beijing.aliyuncs.com/'
     MNS_SECURITY_TOKEN = ''
-    SUBSCRIBE_QUEUE_NAME = 'new-zeus-test'
+    SUBSCRIBE_QUEUE_NAME = 'test-topic'
     MESSAGE_DEBUG_MODE = True
 
 # BDD_SERVER相关配置
