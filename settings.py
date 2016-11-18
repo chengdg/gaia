4950caad297d4cfe3df17c79d8878e9bc2b83341
朱天琦 <zhutianqi@weizoom.com>
2016-11-18 14:41:37 +0800

    修正配置

--- settings.py
+++ settings.py
@@ -157,7 +157,7 @@ else:
     MNS_ACCESS_KEY_SECRET = 'bPKU71c0cfrui4bWgGPO96tLiOJ0PZ'
     MNS_ENDPOINT = 'https://1615750970594173.mns.cn-beijing.aliyuncs.com/'
     MNS_SECURITY_TOKEN = ''
-    SUBSCRIBE_QUEUE_NAME = 'local-test-gaia'
+    SUBSCRIBE_QUEUE_NAME = 'test-order-trade-center'
 
 if 'develop' != MODE:
     MESSAGE_DEBUG_MODE = False
