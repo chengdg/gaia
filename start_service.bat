73d27d7bf4539cf8b2a6cb50fa34659c24034a4c
robert <chenru@weizoom.com>
2016-11-28 13:43:58 +0800

    在start_service.bat中加入启动时注册service的步骤

--- start_service.bat
+++ start_service.bat
@@ -4,7 +4,7 @@ if "%1" == "" (
 	set PORT=%1
 )
 
-::set _REGISTER_SERVICE=1
-::python -c "import servicecli; servicecli.register_service(%PORT%)"
+set _REGISTER_SERVICE=1
+python -c "import servicecli; servicecli.register_service(%PORT%)"
 
 python manage.py runserver 0.0.0.0 %PORT%
