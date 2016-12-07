709e959b70066cdb0a4c1f4668e86dfc67920d3a
aix <aixiang@weizoom.com>
2016-12-07 18:18:06 +0800

    fix

--- api/mall/label/__init__.py
+++ api/mall/label/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 
+import a_product_label
 import a_product_label_group
 import a_product_label_groups
--- business/mall/label/__init__.py
+++ business/mall/label/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import product_label
+import product_label_repository
+import product_label_group
+import product_label_group_repository
