732ba27603147b3712f672a3387b7bc9c043b73b
aix <aixiang@weizoom.com>
2016-12-08 19:11:42 +0800

    update step

--- features/steps/mall_label_steps.py
+++ features/steps/mall_label_steps.py
@@ -22,8 +22,15 @@ def step_impl(context, user):
 	response = context.client.get('/mall/product_label_groups/')
 	actual = response.data['product_label_groups']
 	for data in expected:
+		tmp_list = []
 		if data['labels'] == '':
-			data['labels'] = []
+			data['labels'] = tmp_list
+		else:
+			for label in data['labels'].split(','):
+				tmp_list.append({
+					'label_name': label
+				})
+			data['labels'] = tmp_list
 	bdd_util.assert_list(expected, actual)
 
 @when(u"{user}删除商品标签分组")
@@ -35,6 +42,45 @@ def step_impl(context, user):
 			__del_product_label_group(context, label_group_name)
 	else:
 		__del_product_label_group(context, label_groups_data['label_group_name'])
+
+@when(u"{user}添加商品标签")
+def step_impl(context, user):
+	labels_data = json.loads(context.text)
+	if isinstance(labels_data, list):
+		for label_group_dict in labels_data:
+			label_group_name = label_group_dict['label_group_name']
+			label_name_list = label_group_dict['labels']
+			for label_name in label_name_list:
+				__add_product_label(context, label_group_name, label_name)
+	else:
+		label_name_list = labels_data['labels']
+		for label_name in label_name_list:
+			__add_product_label(context, labels_data['label_group_name'], label_name)
+
+@when(u"{user}删除商品标签")
+def step_impl(context, user):
+	labels_data = json.loads(context.text)
+	if isinstance(labels_data, list):
+		for label_group_dict in labels_data:
+			label_name = label_group_dict['label_name']
+			__del_product_label(context, label_name)
+	else:
+		__del_product_label(context,  labels_data['label_name'])
+
+def __del_product_label(context, label_name):
+	label_id = label_name2id(label_name)
+	response = context.client.delete('/mall/product_label/', {
+		'label_id': label_id,
+	})
+	bdd_util.assert_api_call_success(response)
+
+def __add_product_label(context, label_group_name, label_name):
+	label_group_id = label_group_name2id(label_group_name)
+	response = context.client.put('/mall/product_label/', {
+		'label_group_id': label_group_id,
+		'label_name': label_name
+	})
+	bdd_util.assert_api_call_success(response)
 
 def __add_product_label_group(context, label_group_name):
 	response = context.client.put('/mall/product_label_group/', {
@@ -52,3 +98,7 @@ def __del_product_label_group(context, label_group_name):
 def label_group_name2id(label_group_name):
 	label_group = mall_models.ProductLabelGroup.select().dj_where(name=label_group_name).get()
 	return label_group.id
+
+def label_name2id(label_name):
+	label = mall_models.ProductLabel.select().dj_where(name=label_name).get()
+	return label.id
