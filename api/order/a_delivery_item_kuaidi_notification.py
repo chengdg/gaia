4463458fbc54885aa3a6895059862e7a678d5e3a
“chengdegong” <chengdegong@weizoom.com>
2016-12-13 14:15:30 +0800

    添加短信发送和快递订阅的接口

---
+++ api/order/a_delivery_item_kuaidi_notification.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from eaglet.core import api_resource
+from eaglet.decorator import param_required
+
+from business.order.encode_delivery_item_service import EncodeDeliveryItemService
+from business.mall.express.express_service import ExpressService
+
+
+class ADeliveryItemKuaidiNotification(api_resource.ApiResource):
+	"""
+	快递订阅
+	"""
+	app = 'delivery_item'
+	resource = 'NotifyKuaidi'
+
+	@param_required(['delivery_item_id'])
+	def put(args):
+		"""
+		根据出货单订阅快递
+		"""
+
+		corp = args['corp']
+		delivery_item_repository = corp.delivery_item_repository
+
+		delivery_item_id = args['delivery_item_id']
+
+		delivery_item = delivery_item_repository.get_delivery_item(delivery_item_id, {})
+
+		if delivery_item.with_logistics_trace:
+			# 发送快递订阅
+			is_success = ExpressService(delivery_item).get_express_poll()
+			if not is_success:
+				raise Exception(u'快递订阅异常')
+		return {}
+
+	
