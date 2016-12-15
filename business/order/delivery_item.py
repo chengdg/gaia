efc38c9d70e7e6c1175d22c3e7744f0887ee419e
“chengdegong” <chengdegong@weizoom.com>
2016-12-15 16:06:45 +0800

    修改短信发送的代码

--- business/order/delivery_item.py
+++ business/order/delivery_item.py
@@ -609,6 +609,13 @@ class DeliveryItem(business_model.Model):
 
 		if self.has_db_record:
 			message_content = u"您好，订单号：%s，收货人：%s。已退单，请知晓！【微众传媒】"
-			supplier_tel = self.supplier_info['supplier_tel']		
-			if supplier_tel:
-				send_phone_captcha(supplier_tel, message_content % (self.bid, self.ship_name))
+			supplier_tel = self.supplier_info['supplier_tel']
+			data = {
+                    "phones": str(supplier_tel),
+                    "content": {
+                        "order_id": self.bid,
+                        "ship_name": self.ship_name
+                    },
+                    "sms_code": "SMS_34465265"
+                }		
+			rs = send_phone_captcha(data)
