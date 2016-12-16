a6ca8b0f850a9103ea65d44218704fa2ea1bf729
“chengdegong” <chengdegong@weizoom.com>
2016-12-16 17:12:52 +0800

    修改物流信息代码

--- business/mall/express/express_poll.py
+++ business/mall/express/express_poll.py
@@ -84,7 +84,7 @@ class ExpressPoll(object):
 		"""
 		# post中的param的json
 		param_json_data = {		
-			self.express_params.COMPANY: self.express_company_name_value,
+			self.express_params.COMPANY: self.express_company_name,
 			self.express_params.NUMBER: self.express_number,
 			self.express_params.TO: self.area,
 			self.express_params.KEY: self.express_config.app_key, 
