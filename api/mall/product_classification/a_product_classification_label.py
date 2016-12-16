db317a141aed4f877a5fceddc03e31c9b26e50c3
aix <aixiang@weizoom.com>
2016-12-16 14:55:29 +0800

    fix

--- api/mall/product_classification/a_product_classification_label.py
+++ api/mall/product_classification/a_product_classification_label.py
@@ -32,19 +32,25 @@ class AProductClassificationLabel(api_resource.ApiResource):
 		classification_label_models = classification.get_classification_labels()
 
 		all_label_ids = []
+		label_id2classifi = dict()
 		for model in classification_label_models:
-			all_label_ids.append(model.label_id)
+			label_id = model.label_id
+			all_label_ids.append(label_id)
+			label_id2classifi[label_id] = model.classification_id
 
 		label_models = weizoom_corp.product_label_repository.get_labels(all_label_ids)
 
-		label_group_has_label = {}
+		label_group_has_label = dict()
+		label_relation = dict()
 		for model in label_models:
 			label_group_id = model.label_group_id
-			label_id = model.id
+			label_id = str(model.id)
 			if not label_group_has_label.has_key(label_group_id):
 				label_group_has_label[label_group_id] = [label_id]
 			else:
 				label_group_has_label[label_group_id].append(label_id)
+
+			label_relation[label_id] = True if int(label_id2classifi[label_id]) == int(classification_id) else False
 
 		return_data = []
 		for label_group_id, label_ids in label_group_has_label.items():
@@ -52,7 +58,10 @@ class AProductClassificationLabel(api_resource.ApiResource):
 				'labelGroupId': label_group_id,
 				'labelIds': list(set(label_ids)) #去重
 			})
-		return return_data
+		return {
+			'selected_labels': return_data,
+			'label_relation': label_relation
+		}
 
 	@param_required(['classification_id', 'selected_labels:json'])
 	def put(args):
