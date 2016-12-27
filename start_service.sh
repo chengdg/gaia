92c206b76df86b451b7dfe029c7c6f2b00844d46
robert <chenru@weizoom.com>
2016-12-27 18:15:12 +0800

    support customize uwsgi socket port

--- start_service.sh
+++ start_service.sh
@@ -11,9 +11,16 @@ if [ "${CAN_START_SERVICE}" == "false" ]; then
 	exit 2
 fi
 
+if [ "${_SERVICE_PORT}" == "" ]; then
+	__IGNORE=1 #do nothing
+else
+	PORT=${_SERVICE_PORT}
+fi
+
 # start service
 if [ "$_USE_WSGI_PROTOCAL" == "1" ]; then
-	uwsgi service.ini
+	uwsgi --socket 0.0.0.0:${PORT} service.ini
+else
 else
 	python manage.py runserver 0.0.0.0 $PORT
 fi
