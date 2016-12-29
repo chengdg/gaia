8d4a252a309a013920186c29d6737708cba02a38
charles <qiaozhanlei@weizoom.com>
2016-12-29 14:54:22 +0800

    增加已删除商品处理商品池，备用

---
+++ commands/upgrade_not_deleted_product_db.py
@@ -0,0 +1,36 @@
+﻿# -*- coding: utf-8 -*-
+# __author__ = 'robert'
+import datetime
+import array
+
+from util.command import BaseCommand
+from db.mall import models as mall_models
+
+
+class Command(BaseCommand):
+	help = "python manage.py upgrade_product_db"
+	args = ''
+
+	def handle(self, **options):
+		pooled_products = set([pooled_product.product_id for pooled_product in mall_models.ProductPool.select()])
+		for product in mall_models.Product.select().dj_where(is_deleted=True):
+			if not product.id in pooled_products:
+				print 'add %s' % product.name
+				# 0:下架（待售） 1:上架（在售） 2:回收站
+				# if product.shelve_type == 0:
+				# 	status = mall_models.PP_STATUS_OFF
+				# elif product.shelve_type == 1:
+				# 	status = mall_models.PP_STATUS_ON
+				# elif product.shelve_type == 2:
+				# 	status = mall_models.PP_STATUS_DELETE
+				# else:
+				# 	status = mall_models.PP_STATUS_ON_POOL
+				mall_models.ProductPool.create(
+					woid=product.owner_id,
+					product_id=product.id,
+					status=mall_models.PP_STATUS_DELETE,
+					display_index=9999999,
+					type=mall_models.PP_TYPE_CREATE
+				)
+
+
