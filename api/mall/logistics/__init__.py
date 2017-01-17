ac62df752328c4ec2460550f057e65a4043088e4
zhupenghui <zhupenghui@weizoom.com>
2017-01-17 16:19:27 +0800

    添加电子面单账号

--- api/mall/logistics/__init__.py
+++ api/mall/logistics/__init__.py
@@ -9,6 +9,8 @@ import a_express_delivery
 import a_express_delivery_display_index
 import a_express_deliveries
 import a_express_delivery_companies
+import a_express_bill_account
+import a_express_bill_accounts
 
 import a_limit_zone
 import a_limit_zones
@@ -17,3 +19,4 @@ import a_province_city
 import a_shipper
 import a_shippers
 import a_active_shipper
+
---
+++ api/mall/logistics/a_express_bill_account.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import json
+
+from eaglet.core import api_resource
+from eaglet.decorator import param_required
+from eaglet.core import watchdog
+from eaglet.core.exceptionutil import unicode_full_stack
+
+from business.mall.logistics.express_bill_account import ExpressBillAccount
+
+
+class AExpressBillAccount(api_resource.ApiResource):
+	app = 'mall'
+	resource = 'express_bill_account'
+
+	@param_required(['corp_id', 'id'])
+	def get(args):
+		corp = args['corp']
+
+		express_bill_account = corp.express_bill_account_repository.get_express_bill_account(args['id'])
+
+		data = {
+				'id': express_bill_account.id,
+				'express_name': express_bill_account.express_name,
+				'customer_name': express_bill_account.customer_name,
+				'customer_pwd': express_bill_account.customer_pwd,
+				'logistics_number': express_bill_account.logistics_number,
+				'sendsite': express_bill_account.sendsite,
+				'remark': express_bill_account.remark,
+			}
+
+		return { 'express_bill_account': data }
+
+	@param_required(['corp_id','express_name','customer_name','customer_pwd','logistics_number','sendsite','remark'])
+	def put(args):
+
+		express_bill_account = ExpressBillAccount.create({
+			'express_name' : args['express_name'],
+			'customer_name' : args['customer_name'],
+			'customer_pwd' : args['customer_pwd'],
+			'logistics_number' : args['logistics_number'],
+			'sendsite' : args['sendsite'],
+			'remark' : args['remark'],
+		})
+		return {}
+
+	@param_required(['corp_id','id','express_name','customer_name','customer_pwd','logistics_number','sendsite','remark'])
+	def post(args):
+		corp = args['corp']
+
+		express_bill_account = corp.express_bill_account_repository.get_express_bill_account(args['id'])
+		express_bill_account.update(args['express_name'],args['customer_name'],args['customer_pwd'],args['logistics_number'],args['sendsite'],args['remark']
+		)
+		return {}
+
+	@param_required(['corp', 'id'])
+	def delete(args):
+		corp = args['corp']
+		express_bill_account = corp.express_bill_account_repository.delete_express_bill_account(args['id'])
