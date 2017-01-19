b124a37162dad0f6a11b9a18d12d64d4bb519134
aix <aixiang@weizoom.com>
2017-01-19 14:59:45 +0800

    fix bug

--- api/product/a_pre_product.py
+++ api/product/a_pre_product.py
@@ -42,9 +42,13 @@ class APreProduct(api_resource.ApiResource):
 			'limit_zone_type': pre_product.limit_zone_type,
 			'limit_zone': pre_product.limit_zone,
 			'has_same_postage': pre_product.has_same_postage,
+			'has_multi_models': pre_product.has_multi_models,
 			'postage_money': '%.2f' % pre_product.unified_postage_money,
 			'classification_id': pre_product.classification_id,
-			'classification_name_nav': pre_product.classification_nav
+			'classification_name_nav': pre_product.classification_nav,
+			'status': pre_product.status,
+			'is_accepted': pre_product.is_accepted,
+			'is_updated': pre_product.is_updated
 		}
 
 	@param_required(['corp_id', 'base_info:json', 'models_info:json', 'image_info:json', 'logistics_info:json'])
