879ea9c97bfbb3a8a2d2ff6b4912f00bb8c33858
aix <aixiang@weizoom.com>
2017-02-06 17:28:12 +0800

    mod desc

--- business/product/product.py
+++ business/product/product.py
@@ -214,7 +214,7 @@ class Product(business_model.Model):
 
 	def update_product_unverified(self, args):
 		"""
-		更新编辑审核通过后的商品信息
+		编辑商品信息(未审核)
 		"""
 		product_id = self.id
 		product_data = json.dumps({
@@ -244,7 +244,7 @@ class Product(business_model.Model):
 
 	def verify_modifications(self):
 		"""
-		审核通过商品编辑内容
+		审核通过商品的编辑内容
 		"""
 		product_id = self.id
 		product_data = json.loads(mall_models.ProductUnverified.select().dj_where(product_id=product_id).get().product_data)
