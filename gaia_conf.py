1271e80af5dd05163021fe8013ad00be534652d5
“chengdegong” <chengdegong@weizoom.com>
2017-02-09 18:42:36 +0800

    修改短信发送的主题

--- gaia_conf.py
+++ gaia_conf.py
@@ -12,7 +12,7 @@ TOPIC = {
 	'delivery_item': 'test-topic',
 	'mall_config': "test-topic",
 	'base_service': 'test-topic',  # 基础的异步化服务，如邮件，模板消息等
-	'notify': 'test-phone'  # 短信消息
+	'notify': 'test-topic'  # 短信消息
 }
 
 # 消息队列message_name
