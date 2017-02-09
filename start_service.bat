04cfc2aaa1b543cd369eba485a8b6296d6817327
robert <chenru@weizoom.com>
2017-02-09 19:47:00 +0800

    改进对event service的支持

--- start_service.bat
+++ start_service.bat
@@ -4,6 +4,9 @@ if "%1" == "" (
 	set PORT=%1
 )
 
+pip install -U git+https://git2.weizzz.com:84/microservice/eaglet.git
+pip install -U git+https://git2.weizzz.com:84/microservice/mns_python_sdk.git
+
 set _REGISTER_SERVICE=1
 python -c "import servicecli; servicecli.register_service(%PORT%)"
 
