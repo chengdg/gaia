67205538df4c9e81b2acb77866526a48c4097b1f
robert <chenru@weizoom.com>
2017-03-03 12:55:28 +0800

    添加a_member_grades接口

---
+++ api/member/a_member_grades.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import json
+
+from eaglet.core import api_resource
+from eaglet.core import watchdog
+from eaglet.decorator import param_required
+
+from business.member.social_account import SocialAccount
+from business.member.member import Member
+
+
+class AMemberGrades(api_resource.ApiResource):
+    """
+    获取corp的member grade集合
+    """
+    app = "member"
+    resource = "member_grades"
+
+    @param_required(['corp_id'])
+    def get(args):
+        corp = args['corp']
+        member_grades = corp.member_grade_repository.get_member_grades()
+
+        datas = []
+        for member_grade in member_grades:
+            datas.append({
+                'id': member_grade.id,
+                'name': member_grade.name,
+                'is_default_grade': member_grade.is_default_grade,
+                'is_auto_upgrade': member_grade.is_auto_upgrade,
+                'pay_money': member_grade.pay_money,
+                'pay_times': member_grade.pay_times
+            })
+        
+        return {
+            'member_grades': datas
+        }
