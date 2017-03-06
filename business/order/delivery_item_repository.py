013b5e857939657d75a1426c14b53f9b8470882a
aix <aixiang@weizoom.com>
2017-03-06 18:35:47 +0800

    update

--- business/order/delivery_item_repository.py
+++ business/order/delivery_item_repository.py
@@ -2,6 +2,8 @@
 """
 出货单
 """
+import json
+
 from eaglet.core import paginator
 from eaglet.core import watchdog
 from eaglet.decorator import cached_context_property
@@ -41,9 +43,40 @@ class DeliveryItemRepository(business_model.Model):
 		else:
 			return None
 
+	def __get_db_models_for_corp(self):
+		return mall_models.Order.select().dj_where(supplier=self.corp.id, origin_order_id__gt=0)
+
+	def __search(self, filters):
+		db_models = self.__get_db_models_for_corp()
+		if filters:
+			if '__f-bid-equal' in filters:
+				db_models = db_models.dj_where(order_id=filters['__f-bid-equal'])
+			if '__f-product_name-contain' in filters:
+				order_relations = mall_models.OrderHasProduct.select().dj_where(
+					product_name__icontains=filters['__f-product_name-contain'])
+				ids = [o.order_id for o in order_relations]
+				db_models = db_models.dj_where(id__in=ids)
+			if '__f-status-in' in filters:
+				status = mall_models.MEANINGFUL_WORD2ORDER_STATUS[json.loads(filters['__f-status-in'])[0]]
+				status_params = []
+
+				if status == mall_models.ORDER_STATUS_REFUNDING:
+					status_list = [mall_models.ORDER_STATUS_REFUNDING, mall_models.ORDER_STATUS_GROUP_REFUNDING]
+				elif status == mall_models.ORDER_STATUS_REFUNDED:
+					status_list = [mall_models.ORDER_STATUS_REFUNDED, mall_models.ORDER_STATUS_GROUP_REFUNDED]
+				else:
+					status_list = [status]
+				status_params.extend(status_list)
+
+				db_models = db_models.dj_where(status__in=status_params)
+			if '__f-created_at-range' in filters:
+				filters['__f-created_at-range'] = json.loads(filters['__f-created_at-range'])
+				db_models = db_models.dj_where(created_at__range=filters['__f-created_at-range'])
+
+			return db_models
 
 	def get_delivery_items(self, filters, page_info, fill_options=None):
-		db_models = mall_models.Order.select().dj_where(supplier=self.corp.id, origin_order_id__gt=0)
+		db_models = self.__search(filters)
 		pageinfo, db_models = paginator.paginate(db_models, page_info.cur_page, page_info.count_per_page)
 		delivery_items = DeliveryItem.from_models(
 			{"models": db_models, 'fill_options': fill_options, 'corp': self.corp})
@@ -61,12 +94,6 @@ class DeliveryItemRepository(business_model.Model):
 			return delivery_items[0]
 		else:
 			return None
-
-	def __get_db_models_for_corp(self):
-		webapp_id = self.corp.webapp_id
-		db_models = mall_models.Order.select().dj_where(webapp_id=webapp_id, webapp_user_id__gt=0)
-
-		return db_models
 
 	def get_delivery_items_by_order_id(self, order_id):
 		db_models = self.__get_db_models_for_corp()
