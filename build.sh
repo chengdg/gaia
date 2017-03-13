bf2643d1f55368e1b6d27f4bad0db5007bcb26e2
fanweiming <fanweiming>
2017-03-13 14:11:36 +0800

    add

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ TIMESTAMP_TAG=reg.weizom.com/weizoom/gaia:$VERSION
 docker images --format="{{.Repository}}:{{.Tag}}" | grep gaia | xargs docker rmi
 docker build --no-cache --rm -t ${NAME} -t ${TIMESTAMP_TAG} .
 
-docker push ${NAME} ${TIMESTAMP_TAG}
+docker push ${NAME} 
+docker push ${TIMESTAMP_TAG}
