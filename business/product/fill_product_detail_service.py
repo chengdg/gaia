90095408f3508f1c83baa2990ef57f96e4b315bf
aix <aixiang@weizoom.com>
2017-03-24 14:30:45 +0800

    修复供货商数据问题

--- business/product/fill_product_detail_service.py
+++ business/product/fill_product_detail_service.py
@@ -339,11 +339,9 @@ class FillProductDetailService(business_model.Service):
 		})
 
 		suppliers = resp['data']
-		supplier_corp_id2info = {s['corp_id']: s for s in suppliers}
-		print supplier_corp_id2info
+		supplier_corp_id2info = {int(s['corp_id']): s for s in suppliers}
 
 		for product in products:
-			print product.owner_id, type(product.owner_id)
 			supplier_data = supplier_corp_id2info.get(product.owner_id)
 			product.supplier_info = {
 				'supplier_name': supplier_data['name'],
